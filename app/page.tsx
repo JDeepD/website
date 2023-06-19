caaca281fbcbd532c50acd90a046b0a258518554
Jaydeep Das <jaydeepjd.8914@gmail.com>
2023-06-19 19:36:01 +0530

    minor touch

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ProfileCard from "./components/ProfileCard/ProfileCard";
 export default function Home() {
   return (
     <main className="w-screen h-screen">
-      <div className="flex w-full h-full items-center justify-center">
+      <div className="flex w-full h-full items-center justify-center overflow-hidden">
         <ProfileCard />
       </div>
     </main>
