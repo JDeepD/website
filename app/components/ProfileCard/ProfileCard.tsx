6ab3c2230fb247402fca225f816d805177a75c53
Jaydeep Das <jaydeepjd.8914@gmail.com>
2023-06-19 19:08:34 +0530

    Made responsive

--- app/components/ProfileCard/ProfileCard.tsx
+++ app/components/ProfileCard/ProfileCard.tsx
@@ -41,7 +41,7 @@ export default function ProfileCard() {
   return (
     <motion.div
       whileHover={{ scale: 1.2 }}
-      whileTap={{ scale: 1.1 }}
+      whileTap={{ scale: 1.18 }}
       transition={{ type: "spring", stiffness: 400, damping: 17 }}
     >
       <motion.div
@@ -54,8 +54,7 @@ export default function ProfileCard() {
         }}
         className={styles.container}
       >
-        <div className="flex justify-between px-5 pt-2">
-          <span className={styles.name}>{date.toJSON().slice(0, 10)}</span>
+        <div className="flex justify-center px-5 pt-2">
           <span className={styles.name} onClick={() => setSelected(() => true)}>
             <GlitchText original="JDeepD" text={jdeepd} setText={setJdeepd} />
           </span>
